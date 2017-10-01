c6a28b024f48d06de3a1b80aac89863b762faa2d
Unknown <ncarbonara235@gmail.com>
2017-10-01 01:03:01 -0400

    Started "Collect Thought Code"
    In other words, the code that allows the player to collect a Thought object by pressing the mouse button while in it's vicinity.

--- Assets/Scripts/StrayThoughtMovement.cs
+++ Assets/Scripts/StrayThoughtMovement.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StrayThoughtMovement : MonoBehaviour {
+public class StrayThoughtMovement : MonoBehaviour
+{
 
 	Rigidbody rbody;
 
+	public GameObject player;
+
+	public float distanceToCollect;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rbody = this.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		//if (player is close to thought && player press a button/aims the mouse at the thought/whatever) {
-		//	destroy thought;
-		// give player points or other goodies;
-	//}
+	void Update ()
+	{
+		if (Vector3.Distance (player.GetComponent<Transform> ().position, this.GetComponent<Transform> ().position) < distanceToCollect
+			&& Input.GetKey(KeyCode.Mouse0)) {
+			//	destroy thought;
+			// give player points or other goodies;
+			Destroy(this);
+		}
 	}
 
 	//Update called once every physics framw
-	void FixedUpdate() {
+	void FixedUpdate ()
+	{
 		rbody.velocity = new Vector3 (Random.Range (-5, 5), Random.Range (-5, 5), Random.Range (-5, 5));
 	}
 }
