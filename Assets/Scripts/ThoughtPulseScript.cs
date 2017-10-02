96842630a4c80126e704027ccb0e56faba44d3f8
Unknown <ncarbonara235@gmail.com>
2017-10-01 22:57:52 -0400

    Refined the Thought Explosion Trigger & Added UI Text
    The thought explosion trigger now destroys itself after one use, instantiates objects around it's postion, and prompts UI text to appear.

--- Assets/Scripts/ThoughtPulseScript.cs
+++ Assets/Scripts/ThoughtPulseScript.cs
@@ -2,25 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThoughtPulseScript : MonoBehaviour {
+public class ThoughtPulseScript : MonoBehaviour
+{
 
 	public GameObject thought;
 	public int numberOfThoughts;
+	Vector3 triggerPosition;
+	public GameObject inspirationAlertText;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		triggerPosition = this.GetComponent<Transform>().position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
-	void OnTriggerEnter() {
+	void OnTriggerEnter ()
+	{
+		inspirationAlertText.SetActive(true);
 
 		for (int i = 0; i < numberOfThoughts; i++) {
-		Instantiate (thought);
+			//The following three floats are used spawn the Thought objects at random distances from the center of the trigger,
+			//Rather than directly at it's center
+			float randomSpawnOffsetX = Random.Range (-2, 2);
+			float randomSpawnOffsetY = Random.Range (-2, 2);
+			float randomSpawnOffsetZ = Random.Range (-2, 2);
+
+			//Spawns a new thought object
+			Instantiate (thought, new Vector3(triggerPosition.x + randomSpawnOffsetX, 
+				triggerPosition.y + randomSpawnOffsetY, 
+				triggerPosition.z + randomSpawnOffsetZ), Quaternion.identity);
+		}
+
+		//Destroys the trigger once it has been used, so that it can't be used again
+		GameObject.Destroy (this);
 	}
-}
 }
